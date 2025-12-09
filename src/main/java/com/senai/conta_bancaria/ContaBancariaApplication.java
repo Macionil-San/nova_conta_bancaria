12ce3756edf8bdb1d76b84ffcf7b08279c2436d2
Ruan Macionil <macionilruan@gmail>
2025-12-09 09:33:51 -0300

    add spring-mqttx-0.0.1.jar

--- src/main/java/com/senai/conta_bancaria/ContaBancariaApplication.java
+++ src/main/java/com/senai/conta_bancaria/ContaBancariaApplication.java
@@ -2,7 +2,8 @@ package com.senai.conta_bancaria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+                                                                                //MQTT não está funcinando corretamente
+                                                                                // dependendo da maquina ( not ta indo)
 @SpringBootApplication
 public class ContaBancariaApplication {
     public static void main(String[] args) {
